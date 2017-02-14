8ac7c1c116726268c9ee73637915115a2e1a41f2
kaluznyt <tkaluzny@outlook.com>
2017-02-14 23:33:43 +0100

    commit

---
+++ src/Angular2DotNetCoreSpa/Controllers/PartialController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Angular2DotNetCoreSpa.Controllers
+{
+    public class PartialController : Controller
+    {
+        public IActionResult AboutComponent() => PartialView();
+
+        public IActionResult AppComponent() => PartialView();
+
+        public IActionResult ContactComponent() => PartialView();
+
+        public IActionResult IndexComponent() => PartialView();
+    }
+}
